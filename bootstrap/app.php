dc4b4e724579c7f6784f38350209d3443ac88860
Adrien Neveu <adrien.neveu@epita.fr>
2017-02-25 23:12:38 +0100

    Add Dingo API package

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -82,6 +82,8 @@ $app->singleton(
 // $app->register(App\Providers\AuthServiceProvider::class);
 // $app->register(App\Providers\EventServiceProvider::class);
 
+$app->register(Dingo\Api\Provider\LumenServiceProvider::class);
+
 /*
 |--------------------------------------------------------------------------
 | Load The Application Routes
