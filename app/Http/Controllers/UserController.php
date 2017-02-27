1d475ee4bb8c34c84b6b34c62e9fc4efaf6191fc
Adrien Neveu <adrien.neveu@epita.fr>
2017-02-27 18:28:01 +0100

    Add documentation for /user endpoint

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -1,17 +1,37 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use Dingo\Api\Routing\Helpers;
 use Illuminate\Routing\Controller;
 
+/**
+ * User management resource
+ *
+ * @Resource("User", uri="/user")
+ */
 class UserController extends Controller
 {
+    
     use Helpers;
     
     public function __construct()
     {
     }
     
+    /**
+     * Show user
+     *
+     * Get a JSON representation of the current authenticated user.
+     *
+     * @Get("/")
+     * @Versions({"v1"})
+     * @Transaction({
+     *      @Request(headers={"Authorization": "Bearer AccessToken"}),
+     *      @Response(200, body={"user": {"id": 10, "email": "john@doe.com", "created_at": "2017-02-27 16:48:21"}}),
+     *      @Response(401, body={"message": "Unauthorized", "status_code": 401})
+     * })
+     */
     public function index()
     {
         return $this->auth->user();
